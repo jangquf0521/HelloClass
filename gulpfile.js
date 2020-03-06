2607ba7d6bdb256792dd3adebee18c5759b2cf9c
whalsanr <alsanr@naver.com>
2020-03-06 18:01:45 +0900

    0306 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
-var { src, dest, series} = require('gulp');
+var { src, dest, series, parallel, watch} = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
-function test() {
-  return src("./*.html").pipe(dest("./dist"));
+function clean() {
+  return del("./dist");
 }
 
-exports.default = series(test);
+function test() {
+  return src("./src/*.html").pipe(dest("./dist/"));
+}
+
+function js() {
+  return src("./src/*.js").pipe(dest("./dist/"));
+}
+
+function sassf() {
+  return src('./src/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(dest('./dist/css'));
+}
+
+function watchFiles() {
+  watch("./src/*.html", test);
+  watch("./src/*.js", js);
+  watch("./src/scss/**/*.scss", sassf);
+}
+
+
+
+exports.clean = series(clean);
+exports.bulid = series(clean, test, sassf, js);
+exports.default = parallel( watchFiles, series(test, sassf, js));
+
